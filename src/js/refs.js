e3bb9306d1acb4fa72b61a536151022b85aa1500
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-10-15 22:53:49 +0300

    delete files

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-  searchForm: document.querySelector('#search-form'),
-  imagesContainer: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-  spiner: document.querySelector('.js-spinner'),
-  load: document.querySelector('[data-load]'),
-};
